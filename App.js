c5b686a6ab1b7966cb0180b436d79f0fd465573f
tweetywei <b05902074@ntu.edu.tw>
2021-06-20 15:35:55 +0800

    add modal to set new album name

--- App.js
+++ App.js
@@ -52,11 +52,11 @@ export default function ImagePickerExample() {
 }
 */
 import React , { useState, useEffect } from 'react';
-import { ScrollView, Alert, Modal, StyleSheet, Text, TouchableHighlight, Pressable, View, Button, Image, TextPropTypes } from 'react-native';
+import { Dimensions, ScrollView, Alert, Modal, StyleSheet, Text, TouchableHighlight, Pressable, View, Button, Image, TextPropTypes } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as ImagePicker from 'expo-image-picker';
-import { DefaultTheme, Button as PaperButton, IconButton, Provider as PaperProvider }  from 'react-native-paper';
+import { TextInput, DefaultTheme, Button as PaperButton, IconButton, Provider as PaperProvider }  from 'react-native-paper';
 import sample from './assets/sample.jpeg';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
@@ -64,19 +64,21 @@ function Home({ navigation }) {
 
   const [albums, setAlbums] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
+  const [createModalVisible, setCreateModalVisible] = useState(false);
   const [editAlbumID, seteditAlbumID] = useState(0);
   const [albumCount, setAlbumCount] = useState(0);
-
+  const [newAlbumName, setNewAlbumName] = useState('New Album');
   let editAlbum = (index) => {
     setModalVisible(true);
     seteditAlbumID(index);
   }
 
   let createAlbum = (context) => {
-    setAlbums(oldAlbum => [{id:albumCount, name: "new album"}, ...oldAlbum]);
+    setAlbums(oldAlbum => [{id:albumCount, name: newAlbumName}, ...oldAlbum]);
     context.setDict(albumCount, []);
     setAlbumCount(oldAlbumCount => oldAlbumCount + 1);
     console.log(context.ImageDict);
+    setCreateModalVisible(false);
   };
 
   React.useLayoutEffect(() => {
@@ -87,7 +89,7 @@ function Home({ navigation }) {
           context=>(
         <IconButton icon='plus'
            color="#65ABD4"
-           onPress={() => createAlbum(context)}></IconButton>
+           onPress={() => setCreateModalVisible(true)}></IconButton>
           )
         }
         </ImageContext.Consumer>
@@ -108,6 +110,7 @@ function Home({ navigation }) {
             }}>
             <View style={styles.centeredView}>
               <View style={styles.modalView}>
+              <View style={styles.modalElementView}>
                 <PaperButton
                   onPress={async () => {
                     setModalVisible(false);
@@ -115,19 +118,46 @@ function Home({ navigation }) {
                   }}>
                   Edit Album
                 </PaperButton>
+                </View>
+                <View style={styles.modalElementView}>
                 <PaperButton
                   onPress={() => {
                   }}>
                   Display Album
                 </PaperButton>
+                </View>
+                <View style={styles.modalElementView}>
                 <PaperButton
                   onPress={() => {
                     setModalVisible(!modalVisible);
                   }}>
                   Hide Modal
                 </PaperButton>
+                </View>
               </View>
             </View>
+          </Modal>
+          <Modal
+              animationType="slide"
+              visible={createModalVisible}>
+              <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <View style={styles.modalElementView}>
+                 <TextInput
+                  value={newAlbumName}
+                  onChangeText={(albumName) => setNewAlbumName(albumName)}
+                  placeholder={'New Album'}
+                />
+                </View>
+                <View style={styles.modalElementView}>
+                <PaperButton
+                  onPress={()=>{createAlbum(context)}}
+                >
+                  Create
+                </PaperButton>
+                </View>
+                </View>
+                </View>
           </Modal>
           <View style={{marginVertical: 10}}>
           <PaperButton
@@ -135,7 +165,7 @@ function Home({ navigation }) {
           >
           </PaperButton>
           </View>
-                {albums.map(alb => (<View style={{marginVertical: 10}}><PaperButton key={alb.id} onPress={()=>{editAlbum({...alb}.id)}}>{alb.name}</PaperButton></View>))}
+                {albums.map(alb => (<View style={{marginVertical: 10}} key={alb.id}><PaperButton key={alb.id} onPress={()=>{editAlbum({...alb}.id)}}>{alb.name}</PaperButton></View>))}
         </View>
         )
       }
@@ -241,6 +271,8 @@ const styles = StyleSheet.create({
     marginTop: 22,
   },
   modalView: {
+    width: Dimensions.get('window').width*0.8,
+    height: Dimensions.get('window').height*0.3,
     margin: 20,
     backgroundColor: 'white',
     borderRadius: 20,
@@ -254,6 +286,11 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
+  },
+  modalElementView:
+  {
+    width: '90%',
+    marginVertical: '3%',
   },
   openButton: {
     backgroundColor: '#F194FF',
